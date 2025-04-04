5d4982c984d28b213695cfdd04f8d48dd05b58d0
Cody-Benson <codyobenson@gmail.com>
2025-04-04 17:26:18 -0500

    added express api code and test suite

--- practice.js
+++ practice.js
@@ -1,18 +1,62 @@
 var express = require('express');
 var app = express();
-var users = [
-    { id: 1, name: 'john' },
-    { id: 2, name: 'cody' }
-];
-app.get('/', function (req, res) {
-    res.send('hello world');
-});
-app.get('/users', function (req, res) {
+app.use(express.json());
+var cody = {
+    name: 'cody',
+    email: 'cody@mail.com'
+};
+var chad = {
+    name: 'chad',
+    email: 'chad@mail.com'
+};
+var users = [cody, chad];
+app.get('/users', function (req, res, next) {
     res.send(users);
 });
-app.get('/users/:id', function (req, res) {
-    res.send(req.params.id);
+app.get('/users/:email', function (req, res, next) {
+    var email = req.params.email;
+    var userFound = users.find(function (item) { return item.email === email; });
+    if (userFound) {
+        res.send(userFound);
+    }
+    else {
+        res.status(404).send('user with email:' + email + ' not found');
+    }
+});
+app.post('/users', function (req, res, next) {
+    var newUser = req.body;
+    var userFound = users.find(function (item) { return item.email === newUser.email; });
+    if (userFound === undefined) {
+        users.push(newUser);
+        res.status(201).send(newUser);
+    }
+    else {
+        res.status(409).send('user with email:' + newUser.email + ' already exists.');
+    }
+});
+app.put('/users/:email', function (req, res, next) {
+    var email = req.params.email;
+    var userIndex = users.findIndex(function (item) { return item.email === email; });
+    if (userIndex === -1) {
+        res.status(404).send('user with email:' + email + ' does not exist.');
+    }
+    else {
+        var userUpdate = req.body;
+        users[userIndex] = userUpdate;
+        res.status(200).send(users[userIndex]);
+    }
+});
+app.delete('/users/:email', function (req, res, next) {
+    var email = req.params.email;
+    var userIndex = users.findIndex(function (item) { return item.email === email; });
+    if (userIndex === -1) {
+        res.status(404).send('user with email:' + email + ' does not exist.');
+    }
+    else {
+        users.splice(userIndex, 1);
+        res.status(200).send('user deleted successfully');
+    }
 });
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('listening on port:' + 3000);
 });
--- practice.ts
+++ practice.ts
@@ -1,24 +1,76 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-let users = 
-[
-    {id:1,name:'john'},
-    {id:2,name:'cody'}
-];
+interface User {
+    name:string,
+    email:string
+}
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
+let cody:User = {
+    name:'cody',
+    email:'cody@mail.com'
+}
 
-app.get('/users',(req,res)=>{
+let chad:User = {
+    name:'chad',
+    email:'chad@mail.com'
+}
+
+let users:User[] = [cody,chad];
+
+
+app.get('/users',(req,res,next)=>{
     res.send(users);
 });
 
-app.get('/users/:id',(req,res) => {
-    res.send(req.params.id);
+app.get('/users/:email',(req,res,next)=>{
+    let email:string = req.params.email;
+    let userFound = users.find((item)=>item.email === email);
+    
+    if(userFound){
+        res.send(userFound);
+    }else{
+        res.status(404).send('user with email:' + email + ' not found');
+    }
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.post('/users',(req,res,next)=>{
+    let newUser:User = req.body;
+    let userFound = users.find((item)=>item.email === newUser.email);
+
+    if(userFound === undefined){
+        users.push(newUser);
+        res.status(201).send(newUser);
+    }else{
+        res.status(409).send('user with email:' + newUser.email + ' already exists.');
+    }
+});
+
+app.put('/users/:email',(req,res,next)=>{
+    let email = req.params.email;
+    let userIndex = users.findIndex((item)=>item.email === email);
+    if(userIndex === -1){
+        res.status(404).send('user with email:' + email + ' does not exist.');
+    }else{
+        let userUpdate:User = req.body;
+        users[userIndex] = userUpdate;
+        res.status(200).send(users[userIndex]);
+    }
+})
+
+app.delete('/users/:email',(req,res,next)=>{
+    let email = req.params.email;
+    let userIndex = users.findIndex((item)=>item.email === email);
+
+    if(userIndex === -1){
+        res.status(404).send('user with email:' + email + ' does not exist.');
+    }else{
+        users.splice(userIndex,1);
+        res.status(200).send('user deleted successfully');
+    }
+});
+
+app.listen(3000,()=>{
+    console.log('listening on port:' + 3000);
 });
